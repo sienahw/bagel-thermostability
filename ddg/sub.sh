3107a7bc85428817a9592088802e8a63701b5044
Alex Carlin <carlin@ucdavis.edu>
2016-02-15 12:03:36 -0800

    Cleaned up and merged this with ddg repo

--- ddg/sub.sh
+++ ddg/sub.sh
@@ -1,2 +1,5 @@
+#!/bin/sh
+#SBATCH --job-name=ddg
+#SBATCH --output=log.txt 
 
-/share/work/rosetta/source/bin/ddg @ flags 
+/share/work/rosetta/source/bin/ddg_monomer.linuxgccrelease @flags $( sed -n "$SLURM_ARRAY_TASK_ID p" list )
