bfe5a6a61d4204468cf5f45a772711e3bef3f413
Alex Carlin <carlin@ucdavis.edu>
2016-02-15 12:35:48 -0800

    Added back in confs file and integrated everything into a single notebook

--- ddg/sub.sh
+++ ddg/sub.sh
@@ -2,4 +2,5 @@
 #SBATCH --job-name=ddg
 #SBATCH --output=log.txt 
 
-/share/work/rosetta/source/bin/ddg_monomer.linuxgccrelease @flags $( sed -n "$SLURM_ARRAY_TASK_ID p" list )
+MUT=$( sed -n "$SLURM_ARRAY_TASK_ID p" list ) 
+srun /share/work/rosetta/source/bin/ddg_monomer.linuxgccrelease @flags -resfile resfiles/$MUT
